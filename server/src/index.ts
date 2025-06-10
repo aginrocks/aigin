32f4a00c9a50f75aba11ca3646f73360ddc0b8de
Tymek <vonautymek@gmail.com>
2025-06-11 00:36:51 +0200

    added oidc

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import { publicProcedure, router } from './trpc';
+import { router } from './trpc';
 import { extendZod } from '@zodyac/zod-mongoose';
 import { z } from 'zod';
 import { initDatabase } from './db';
@@ -6,10 +6,13 @@ import { authRouter } from '@routers/auth';
 import { Hono } from 'hono';
 import { trpcServer } from '@hono/trpc-server';
 import { serve } from '@hono/node-server';
+import dotenv from 'dotenv';
+import { getAuth, oidcAuthMiddleware, processOAuthCallback, revokeSession } from '@hono/oidc-auth';
+
+dotenv.config();
+extendZod(z);
 
 const app = new Hono();
-
-extendZod(z);
 
 initDatabase().then(() => {
     console.log('Database connected successfully');
@@ -30,6 +33,22 @@ app.use(
         endpoint: '/api/trpc',
     })
 );
+
+app.get('/api/logout', async (c) => {
+    await revokeSession(c);
+    return c.text('You have been successfully logged out!');
+});
+
+app.get('/api/auth/callback', async (c) => {
+    return processOAuthCallback(c);
+});
+
+app.get('/api/me', async (c) => {
+    const auth = await getAuth(c);
+    return c.text(`Hello <${auth?.email}>!`);
+});
+
+app.use('*', oidcAuthMiddleware());
 
 const server = serve({
     fetch: app.fetch,
