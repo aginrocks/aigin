5a19cc61acdfc069302ae3f5e0719f3408493f34
Tymek <vonautymek@gmail.com>
2025-06-10 12:54:57 +0200

    initialized trpc

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,17 @@
-import { router } from './trpc';
+import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import { publicProcedure, router } from './trpc';
 
 const appRouter = router({
-    // ...
+    greeting1: publicProcedure.query(() => 'hello tRPC v10!'),
 });
 
 // Export type router type signature,
 // NOT the router itself.
 export type AppRouter = typeof appRouter;
+
+const server = createHTTPServer({
+    router: appRouter,
+    basePath: '/api/trpc/',
+});
+
+server.listen(3001);
