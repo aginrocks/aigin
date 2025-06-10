4e8e27df53210e4b8257fe181e29e76d0a084b07
Tymek <vonautymek@gmail.com>
2025-06-11 00:09:39 +0200

    added hono

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,11 @@ import { extendZod } from '@zodyac/zod-mongoose';
 import { z } from 'zod';
 import { initDatabase } from './db';
 import { authRouter } from '@routers/auth';
+import { Hono } from 'hono';
+import { trpcServer } from '@hono/trpc-server';
+import { serve } from '@hono/node-server';
+
+const app = new Hono();
 
 extendZod(z);
 
@@ -20,9 +25,30 @@ const appRouter = router({
 // NOT the router itself.
 export type AppRouter = typeof appRouter;
 
-const server = createHTTPServer({
-    router: appRouter,
-    basePath: '/api/trpc/',
+app.use(
+    '/api/trpc/*',
+    trpcServer({
+        router: appRouter,
+        endpoint: '/api/trpc',
+    })
+);
+
+const server = serve({
+    fetch: app.fetch,
+    port: 3001,
 });
 
-server.listen(3001);
+// graceful shutdown
+process.on('SIGINT', () => {
+    server.close();
+    process.exit(0);
+});
+process.on('SIGTERM', () => {
+    server.close((err) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        process.exit(0);
+    });
+});
