49952e080fa754261165c26745d8f9760d17a3fd
Tymek <vonautymek@gmail.com>
2025-06-18 21:23:06 +0300

    fixes

---
+++ cluster/traefik/apply.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+kubectl apply -f 00-account.yaml
+kubectl apply -f 00-role.yaml
+kubectl apply -f 01-role-binding.yaml
+kubectl apply -f 02-traefik.yaml
+kubectl apply -f 02-traefik-services.yaml
+kubectl apply -f 03-traefik-ingressclass.yaml
