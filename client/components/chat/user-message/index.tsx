509c7586f7788cce9cb4c5541f4bb907b7827412
Tymek <vonautymek@gmail.com>
2025-06-18 06:21:02 +0200

    Client: css fixes

--- client/components/chat/user-message/index.tsx
+++ client/components/chat/user-message/index.tsx
@@ -6,7 +6,7 @@ export default function UserMessage({ children }: UserMessageProps) {
     return (
         <div className="w-full max-w-4xl min-h-fit flex justify-end">
             <div className="bg-popover/80 border rounded-2xl flex flex-col px-4 py-3 max-w-[80%]">
-                <div className="text-base text-foreground">{children}</div>
+                <div className="text-base text-foreground whitespace-pre-line">{children}</div>
             </div>
         </div>
     );
