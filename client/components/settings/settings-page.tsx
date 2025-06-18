6660e7731cdc198cf9f6c873feea811cfb75af61
Tymek <vonautymek@gmail.com>
2025-06-18 05:05:51 +0200

    Client: fixed paddings in settings

--- client/components/settings/settings-page.tsx
+++ client/components/settings/settings-page.tsx
@@ -6,5 +6,5 @@ export type SettingsPageProps = {
 
 export function SettingsPage({ children }: SettingsPageProps) {
     // pt-4
-    return <div className="pt-4 w-full flex-1 pl-0 pr-4 flex flex-col gap-3">{children}</div>;
+    return <div className="pt-4 w-full flex-1 px-4 flex flex-col gap-3">{children}</div>;
 }
--- client/components/settings/sidebar.tsx
+++ client/components/settings/sidebar.tsx
@@ -8,7 +8,7 @@ export type SettingsSidebarProps = {
 };
 export function SettingsSidebar({ title, children }: SettingsSidebarProps) {
     return (
-        <SecondarySidebar className="w-45 mx-2 py-4  border-r">
+        <SecondarySidebar className="w-45 px-2 py-4 border-r">
             <div className="text-xl font-semibold px-4 pb-2">{title}</div>
             <SidebarList className="gap-1">{children}</SidebarList>
         </SecondarySidebar>
